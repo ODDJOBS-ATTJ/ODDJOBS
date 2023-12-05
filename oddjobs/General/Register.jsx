9cb02acffd61e8d106d224bdcd4587b0e4b932d3
Ditto Tiu <89990545+dittttt@users.noreply.github.com>
2023-12-06 01:16:25 +0800

    All Pages in react (NOT LINKED + no backend)

---
+++ oddjobs/General/Register.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import generalStyles from './CSS/general-styles.module.css';
+import SignedOutHeader from '../General/Signed-Out-Header';
+import RegisterForm from './Backend/RegisterForm';
+
+function Register() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <SignedOutHeader />
+      <div className={generalStyles.container}>
+        <Link className={generalStyles['back-button']} onClick={() => navigate(-1)}>
+          &larr; Back
+        </Link>
+        {/* REGISTER FORM */}
+        <div className={generalStyles['card-form']}>
+          <RegisterForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
---
+++ oddjobs/Views/Worker/template.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { ReactDOM } from "react"
+import { Link } from "react-router-dom"
+import SignedInHeader from "./signedInHeader"
+
+function template(){
+    return(
+        <>
+            <SignedInHeader/>
+        </>
+    );
+}   
+
+export default template;
