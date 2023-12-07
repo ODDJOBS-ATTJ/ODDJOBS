fb4276394197c45af572b9a8da49280ae12d7c2a
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2023-12-07 23:57:28 +0800

    Test change

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended:true }));
 
 app.use(bodyParser.json());
 
-app.use('/account', accountRoutes)
+app.use('/', accountRoutes)
 
 // app.get('/', (req, res) => {
 //     res.send('<h1>HELLO MYTHER</h1>');
