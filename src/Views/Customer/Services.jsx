13cf3856a02d02ae4a5f86edeedb6f9a04c7b401
theprofessorxavier <xavierneilangel@gmail.com>
2023-12-08 21:57:51 +0800

    partial edit on services.jsx

--- src/Views/Customer/Services.jsx
+++ src/Views/Customer/Services.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'; // Add this import
 import styles from './CSS/services.module.css';
 import SignedInHeader from './Signed-In-Header';
 import magnifyingGlass from './IMAGE/Icons/magnifying-glass.png';
@@ -14,9 +15,46 @@ import misc from './IMAGE/icons/misc.png';
 
 function Services() {
     const [type, setType] = useState(0);
+    const [serviceTypes, setServiceTypes] = useState([]);
+
     const chooseType = (num) => {
         setType(num);    
     };
+
+    useEffect(() => {
+      axios.get('http://localhost:3000/serviceType/findAll')
+        .then(response => {
+          setServiceTypes(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching service types:', error);
+        });
+    }, []);
+    
+    // function random(){
+    //     let category;
+
+    //     switch(type){
+    //         case 0: category = 'featured services'; break;
+    //         case 1: category = serviceTypes[0].serviceTypeName; break;
+    //         case 2: category = serviceTypes[1].serviceTypeName; break;
+    //         case 3: category = serviceTypes[2].serviceTypeName; break;
+    //         case 4: category = serviceTypes[3].serviceTypeName; break;
+    //         case 5: category = serviceTypes[4].serviceTypeName; break;
+    //     }
+
+    //     return(
+    //         <div style={{textTransform:"uppercase"}}>
+    //             <h1>{category}</h1>;
+    //         </div> 
+    //     )
+    // }
+    // <h1>Service Types</h1>
+    // <ul>
+    //   {serviceTypes.map(serviceType => (
+    //     <li key={serviceType.serviceTypeID}>{serviceType.serviceTypeName}</li>
+    //   ))}
+    // </ul>
 
     return (
         <div>
@@ -35,7 +73,7 @@ function Services() {
                 </div>
                 <div className={styles['services-container-row']}>
                     <div className={styles['categories-card']}>
-                        <Link to="/customer/services" className={styles['categories']}>
+                        <Link onClick={()=>chooseType(0)}  className={styles['categories']}>
                             <div className={styles['category-img']}>
                                 <img src={featured} alt="Category" />
                             </div>
@@ -47,25 +85,25 @@ function Services() {
                             </div>
                             <h1>home</h1>
                         </Link>
-                        <Link to="/customer/services/categories" className={styles['categories']}>
+                        <Link onClick={()=>chooseType(2)} className={styles['categories']}>
                             <div className={styles['category-img']}>
                                 <img src={event} alt="Category" />
                             </div>
                             <h1>event</h1>
                         </Link>
-                        <Link to="/customer/services/categories" className={styles['categories']}>
+                        <Link onClick={()=>chooseType(3)}  className={styles['categories']}>
                             <div className={styles['category-img']}>
                                 <img src={health} alt="Category" />
                             </div>
                             <h1>health</h1>
                         </Link>
-                        <Link to="/customer/services/categories" className={styles['categories']}>
+                        <Link onClick={()=>chooseType(4)} className={styles['categories']}>
                             <div className={styles['category-img']}>
                                 <img src={mechanical} alt="Category" />
                             </div>
                             <h1>mechanical</h1>
                         </Link>
-                        <Link to="/customer/services/categories" className={styles['categories']}>
+                        <Link onClick={()=>chooseType(5)} className={styles['categories']}>
                             <div className={styles['category-img']}>
                                 <img src={misc} alt="Category" />
                             </div>
@@ -75,8 +113,7 @@ function Services() {
                     </div>
                 </div>
                 <div className={styles['services-container-row']}>
-
-                    <h1></h1>
+                    {/* {random()} */}
                 </div>
                 <div className={styles['services-container-row']}>
                     <div className={styles['featured-services-row']}>
@@ -111,34 +148,8 @@ function Services() {
                 </div>
                 <div className={styles['services-container-row']}>
                     <div className={styles['featured-services-row']}>
-                        <Link to="/Customer/services/details" className={styles['featured-services-col']}>
-                            <div className={styles['featured-services-content']}>
-                                <div className={styles['image']}>
-                                    <img src={samplePhoto} alt="Featured Service" />
-                                </div>
-                                <div className={styles['space']} />
-                                <div className={styles['description']}>
-                                    <h1>TUTOR</h1>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex nisi, congue sit amet varius quis,
-                                        convallis et nulla. Duis ac est diam. Maecenas diam nunc, vestibulum vitae ligula
-                                    </p>
-                                </div>
-                            </div>
-                        </Link>
+                        
                         <div className={styles['featured-services-space']}></div>
-                        <Link to="/Customer/services/details" className={styles['featured-services-col']}>
-                            <div className={styles['featured-services-content']}>
-                                <div className={styles['image']}>
-                                    <img src={samplePhoto} alt="Featured Service" />
-                                </div>
-                                <div className={styles['space']} />
-                                <div className={styles['description']}>
-                                    <h1>HOUSEKEEPING</h1>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex nisi, congue sit amet varius quis,
-                                        convallis et nulla. Duis ac est diam. Maecenas diam nunc, vestibulum vitae ligula </p>
-                                </div>
-                            </div>
-                        </Link>
                     </div>
                 </div>
             </div>
