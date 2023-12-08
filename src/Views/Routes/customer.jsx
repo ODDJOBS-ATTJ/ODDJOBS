6e9fb6a0b606ce4621e6281acc5442a389f77d5d
dit <89990545+dittttt@users.noreply.github.com>
2023-12-09 05:46:53 +0800

    ACCOUNT UPDATED

--- src/Views/Routes/customer.jsx
+++ src/Views/Routes/customer.jsx
@@ -4,7 +4,6 @@ import UserProfile from '../Customer/User-Profile';
 import UserProfileEdit from '../Customer/User-Profile-Edit';
 import Billings from '../Customer/Billings';
 import Services from '../Customer/Services';
-import ServicesCategories from '../Customer/Services-Categories';
 import ServicesSearch from '../Customer/Services-Search';
 import ServicesDetails from '../Customer/Services-Details';
 import Bookings from '../Customer/Bookings';
@@ -18,7 +17,6 @@ function CustomerRoutes() {
       <Route path="/profile/edit" element={<UserProfileEdit />} />
       <Route path="/billings" element={<Billings />} />
       <Route path="/services" element={<Services />} />
-      <Route path="/services/categories" element={<ServicesCategories />} />
       <Route path="/services/search" element={<ServicesSearch />} />
       <Route path="/services/details" element={<ServicesDetails />} />
       <Route path="/bookings" element={<Bookings />} />
