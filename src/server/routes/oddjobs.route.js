abe4b76d52e6a5ed82071e3980ad95259a544b49
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2023-12-08 17:53:05 +0800

    dnf

--- src/server/routes/oddjobs.route.js
+++ src/server/routes/oddjobs.route.js
@@ -7,7 +7,7 @@ router.get('/', accountController.findAll); // GET all account
 router.post('/register', accountController.create); // POST register
 router.post('/login', accountController.login); // POST login
 router.post('/findUserID', accountController.findUserID); // POST login
-router.post('/logout', accountController.logout);
+router.post('/logout', accountController.logout); //LOGOUT
 router.get('/:id', accountController.findById); // GET id account
 router.put('/:id', accountController.update); // PUT id account
 router.delete('/:id', accountController.delete); // DELETE id account
