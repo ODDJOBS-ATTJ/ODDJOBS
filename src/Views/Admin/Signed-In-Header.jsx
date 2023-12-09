772c8e253d5fb8025bcef77c381ed0ad8dbc276a
dit <89990545+dittttt@users.noreply.github.com>
2023-12-09 11:55:24 +0800

    FIXED LINKS

--- src/Views/Admin/Signed-In-Header.jsx
+++ src/Views/Admin/Signed-In-Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './CSS/default.css'
-import { Link } from 'react-router-dom';
-import Ditto from './Icons/ditto.jpg';
+import React, { useState, useEffect } from 'react';
+import styles from './CSS/default.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+import pfp from './Icons/ditto.jpg';
+import { useAuth } from '../../server/useAuth.js';
+import logout from '../../server/logout.js';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
-function SignedInHeader(){
-  const userID = Cookies.get('userID');
+function SignedInHeader() {
+    const navigate = useNavigate();
+    const [authenticated, setAuthenticated] = useState(true); // Initial state based on authentication status
+    
+    useAuth();
+
+    const userID = Cookies.get('userID');
     const [user, setUser] = useState([{}]);
 
     useEffect(() => {
@@ -19,35 +26,51 @@ function SignedInHeader(){
             });
     }, []);
     console.log(user);
+    
 
-  return (
-  <div className="header">
-    <div className="inner_header">
-      <a className="logo_container" href="/General/home/home.html">
-        <div className="logo_box">
-          <h1>ODD<span>JOBS</span></h1>
-        </div>
-      </a>
-      <ul className="nav">
-        <li><Link to="/admin/logs">ADMIN LOG</Link></li>
-        <li><Link to="/admin/applications">APPLICATION</Link></li>
-        <li><Link to="/admin/service-management">SERVICE MANAGEMENT</Link></li>
-        <li><Link to="/admin/user-management">USER MANAGEMENT</Link></li>
-        <li className="user-greeting"><Link to="/admin/profile">Hi, Beneditto</Link></li>
-        <li className="pfp" tabIndex={0}>
-          <div className="dropdown">
-            <img src={Ditto} alt="User Profile" className="dropbtn" />
-            <div className="dropdown-content">
-              <Link to="/admin/profile">User Profile</Link>
-              <div className="line" />
-              <Link to="/" className="signout">Log Out</Link>
+    const handleLogout = async () => {
+        await logout(); // Call the logout function
+
+        // Update the local state to reflect the authentication status
+        setAuthenticated(false);
+
+        // Redirect to the login page
+        navigate('/login');
+    };
+
+    // Conditional rendering based on authentication status
+    if (!authenticated) {
+        return null; // Render nothing if not authenticated
+    }
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.inner_header}>
+                <Link to="/customer/services" className={styles.logo_container}>
+                    <div className={styles.logo_box}>
+                        <h1>ODD<span>JOBS</span></h1>
+                    </div>
+                </Link>
+                <ul className={styles.nav}>
+                    <li><Link to="/admin/logs">ADMIN LOG</Link></li>
+                    <li><Link to="/admin/application">APPLICATION</Link></li>
+                    <li><Link to="/admin/service-management">SERVICE MANAGEMENT</Link></li>
+                    <li><Link to="/admin/user-management">USER MANAGEMENT</Link></li>
+                    <li className={styles['user-greeting']}><Link to="/admin/profile">Hi, {user[0].firstName}</Link></li>
+                    <li className={styles.pfp} tabIndex={0}>
+                        <div className={styles.dropdown}>
+                            <img src={pfp} alt="User Profile" className={styles.dropbtn} />
+                            <div className={styles['dropdown-content']}>
+                                <Link to="/admin/profile">User Profile</Link>
+                                <div className={styles.line} />
+                                <Link onClick={handleLogout} className={styles.signout}>Log Out</Link>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>  
-  );
+        </div>
+    );
 }
 
 export default SignedInHeader;
